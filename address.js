06d4dc24f9d5cdabc7711330faee8b59347c9b23
Kath Johnson <KathJohnson1918@protonmail.com>
2021-07-06 17:05:46 +0100

    add testnet address

--- address.js
+++ address.js
@@ -13,16 +13,16 @@ module.exports = {
     "USDT/BRET MDEX-LP": ""
   },
   "256": {
-    "BRET": "",
+    "BRET": "0xdd010A19259BCdcB018817d5705DE683C2cA9B02",
     // bretUSD is an ERC20 token and also the bretton usd pool
-    "bretUSD": "",
-    "Converter": "",
-    "FeeDistributor": "",
-    "Vault": "",
-    "SwapMiningRewardsCalc": "",
-    "SwapMiningRewards": "",
-    "LiquidityMiningRewards": "",
-    "Treasury": "",
+    "bretUSD": "0x768329eA938655b46d5bFc863c00609bDA39ef3A",
+    "Converter": "0xFC14efaEE74666F60577D62649871F5827E7409F",
+    "FeeDistributor": "0xC8ccb03a98Db0CDDdF3743fF0D9d99aB943D8299",
+    "Vault": "0x5e3a4FE88a10CDa50827e5a451534226A54B0c98",
+    "SwapMiningRewardsCalc": "0x1d8bad3e6908dC62113c99eeCA67e653fD38428a",
+    "SwapMiningRewards": "0xcf14F8634dE4a86240E89D6ea6d3FF8425A29330",
+    "LiquidityMiningRewards": "0xB7b07071ea1f51084b3030dE43B76A40dBd69C87",
+    "Treasury": "0xB6c0aC940AF5408602f75f2A46426b01D4B71188",
     "USDT/BRET MDEX-LP": ""
   }
 }
